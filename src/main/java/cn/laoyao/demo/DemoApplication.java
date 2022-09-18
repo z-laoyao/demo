1d3bfbfeb51cc943f8a11a77d7f083c692634746
laoyao <laoyao777@outlook.com>
2022-09-18 18:55:35 +0800

    ice commit

--- src/main/java/cn/laoyao/demo/DemoApplication.java
+++ src/main/java/cn/laoyao/demo/DemoApplication.java
@@ -9,7 +9,7 @@ public class DemoApplication {
     public static void main(String[] args) {
         System.out.println("aaaaaaaaaaaaa");
         SpringApplication.run(DemoApplication.class, args);
-        //allajadddddddddddojadfafd
+        System.out.println("ice commit");
     }
 
 }
