4b959a16a2732299a81b9f66ed3b510453dfa2e8
laoyao <laoyao777@outlook.com>
2022-09-18 18:47:10 +0800

    third commit

--- src/main/java/cn/laoyao/demo/DemoApplication.java
+++ src/main/java/cn/laoyao/demo/DemoApplication.java
@@ -9,6 +9,7 @@ public class DemoApplication {
     public static void main(String[] args) {
         System.out.println("aaaaaaaaaaaaa");
         SpringApplication.run(DemoApplication.class, args);
+        //allajadddddddddddojadfafd
     }
 
 }
